3b7d65581c40e641f1bd8f49147d59e3768834d4
vedantborse-ai <vedantpborse@gmail.com>
2025-08-10 16:48:03 +0530

    Time: 12 ms (83.98%), Space: 47.2 MB (62.75%) - LeetHub

---
+++ 0943-sum-of-subarray-minimums/0943-sum-of-subarray-minimums.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int sumSubarrayMins(vector<int>& arr) {
+        int n = arr.size();
+        vector<int> nse = findNSE(arr);
+        vector<int> pse = findPSE(arr);
+        long long total = 0;
+        int mod = 1e9 + 7;
+
+        for (int i = 0; i < n; i++) {
+            long long left = i - pse[i];
+            long long right = nse[i] - i;
+            total = (total + (right * left % mod) * arr[i] % mod) % mod;
+        }
+        return (int)total;
+    }
+
+    vector<int> findNSE(vector<int>& arr) {
+        int n = arr.size();
+        vector<int> nse(n, n);
+        stack<int> st;
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.empty() && arr[st.top()] > arr[i]) {
+                st.pop();
+            }
+            nse[i] = st.empty() ? n : st.top();
+            st.push(i);
+        }
+        return nse;
+    }
+
+    vector<int> findPSE(vector<int>& arr) {
+        int n = arr.size();
+        vector<int> pse(n, -1);
+        stack<int> st;
+        for (int i = 0; i < n; i++) {
+            while (!st.empty() && arr[st.top()] >= arr[i]) {
+                st.pop();
+            }
+            pse[i] = st.empty() ? -1 : st.top();
+            st.push(i);
+        }
+        return pse;
+    }
+};
