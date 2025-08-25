fcb2864dc5155a936ea4a33334a02843c15f98db
vedantborse-ai <vedantpborse@gmail.com>
2025-08-25 23:15:34 +0530

    Time: 47 ms (10.41%), Space: 61.5 MB (62.65%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -4,9 +4,7 @@ public:
         priority_queue<int, vector<int>, greater<int>> minHeap;
         for (int num : nums) {
             minHeap.push(num);
-            if ((int)minHeap.size() > k) {
-                minHeap.pop();
-            }
+            if (minHeap.size() > k) minHeap.pop();
         }
         return minHeap.top();
     }
